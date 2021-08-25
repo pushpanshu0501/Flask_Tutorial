1631c8975b2999f688a19357f7ef5212cbcc54eb
pushpanshu0501 <tripathipushpanshu0501@gmail.com>
2021-08-25 15:42:13 +0530

    url updated

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ def root_page():
     return render_template("index.html", name=name)
 
 
-@app.route('/method',methods=['GET', 'POST']) 
-def fun():
-    if request.method == 'POST':
-        return "You have used a POST request"
-    else:
-        return "I reckon you are probably using GET method"
+# @app.route('/method',methods=['GET', 'POST']) 
+# def fun():
+#     if request.method == 'POST':
+#         return "You have used a POST request"
+#     else:
+#         return "I reckon you are probably using GET method"
 
  
 app.run()
